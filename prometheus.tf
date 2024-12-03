c2c857440223bf4d92bded0b3b18cfb48c331723
kostakoff <kostakoff@local>
2024-12-03 12:18:29 +0300

    added prometheus stack

--- prometheus.tf
+++ prometheus.tf
@@ -2,10 +2,10 @@ resource "helm_release" "prometheus" {
   name = "prometheus"
 
   repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
+  chart            = "kube-prometheus-stack"
   namespace        = "prometheus"
   create_namespace = true
-  version          = "26.0.0"
+  version          = "66.3.0"
 
   values = [
     "${templatefile("${path.module}/files/values/prometheus.yaml", {domain = "apps.${var.name}.myltd.lab"})}"
